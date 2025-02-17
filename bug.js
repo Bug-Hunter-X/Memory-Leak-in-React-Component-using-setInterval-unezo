1e27a43970cb89f4a9b7c489fa622a20471e9cf1
BugHunter-X <askzenai@gmail.com>
2025-02-17 17:12:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,20 @@
+```javascript
+function MyComponent() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    // Incorrect usage of setInterval. The callback function needs to return a cleanup function.
+    const intervalId = setInterval(() => {
+      setCount(prevCount => prevCount + 1);
+    }, 1000);
+
+    // Missing cleanup function to clear the interval when the component unmounts
+  }, []);
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+    </div>
+  );
+}
+```
